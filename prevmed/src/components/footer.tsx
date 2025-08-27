1a8e10b8ba7f5d5cfa3e0ce60d34bc0c06f87919
shrinidr <rushilk@bu.edu>
2025-08-27 16:57:19 +0530

    Basic mobile compatibility enabled.

--- prevmed/src/components/footer.tsx
+++ prevmed/src/components/footer.tsx
@@ -6,12 +6,11 @@ const Footer: React.FC = () => {
       <div className="container cols">
         <div className="meta">
           <div className="kicker">Preventive Health</div>
-          <div>View-only prototype. No personal data collected.</div>
+          <div className='patanai'>View-only prototype. No personal data collected.</div>
         </div>
-        <ul>
+        <ul className='ulla'>
           <li><a href="#" aria-label="Twitter">Twitter</a></li>
           <li><a href="#" aria-label="LinkedIn">LinkedIn</a></li>
-          <li><a href="#" aria-label="GitHub">GitHub</a></li>
           <li><a href="#" aria-label="Terms of Service">Terms</a></li>
           <li><a href="#" aria-label="Privacy Policy">Privacy</a></li>
         </ul>
