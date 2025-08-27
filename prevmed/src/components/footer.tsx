3c1e669093a5c3aa4535cc4dbc1f3f2001f92516
shrinidr <rushilk@bu.edu>
2025-08-27 17:18:44 +0530

    done done

--- prevmed/src/components/footer.tsx
+++ prevmed/src/components/footer.tsx
@@ -5,8 +5,8 @@ const Footer: React.FC = () => {
     <footer>
       <div className="container cols">
         <div className="meta">
-          <div className="kicker">Preventive Health</div>
-          <div className='patanai'>View-only prototype. No personal data collected.</div>
+          <div className="kicker">Preventivity </div>
+          <div className='patanai'>Our website services and content is for <br/>informational purposes only. Preventivity does <br/> not provide medical advice, diagnosis, or treatment. </div>
         </div>
         <ul className='ulla'>
           <li><a href="#" aria-label="Twitter">Twitter</a></li>
@@ -14,6 +14,7 @@ const Footer: React.FC = () => {
           <li><a href="#" aria-label="Terms of Service">Terms</a></li>
           <li><a href="#" aria-label="Privacy Policy">Privacy</a></li>
         </ul>
+        <div className='bulla'> Contact us: <a href="mailto:contact@preventivity.org"><b>contact@preventivity.org</b></a></div>
       </div>
     </footer>
   )
