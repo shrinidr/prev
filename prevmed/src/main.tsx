a8171990c76e3cce693bd7d159d61ef5eaa76df5
shrinidr <rushilk@bu.edu>
2025-08-27 17:28:27 +0530

    Added vercel analytics component

--- prevmed/src/main.tsx
+++ prevmed/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 )
